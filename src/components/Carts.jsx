eec3efd51d6bdc0a19fd0e33635aa688f9eec61b
Wahab Khan <kwahab789@gmail.com>
2021-12-07 14:50:42 +0500

    practicing

---
+++ src/components/Carts.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import product from "../data";
+const Carts = () => {
+	return (
+		<>
+			{product.data.map((item) => (
+				<div className="container" key={item.id}>
+					<div className="row">
+						<div
+							className="col-12"
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "center",
+								alignItems: "center",
+								textAlign: "center",
+							}}
+						>
+							<div
+								class="card"
+								style={{
+									width: "18rem",
+									display: "flex",
+									flexDirection: "column",
+									justifyContent: "center",
+									alignItems: "center",
+									textAlign: "center",
+								}}
+							>
+								<img src={item.img} class="card-img-top" alt="..." />
+								<div class="card-body">
+									<h5 class="card-title">{item.cate}</h5>
+									<p class="card-text">{item.title}.</p>
+									<a href="#" class="btn btn-primary">
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			))}
+			{product.data1.map((item) => (
+				<div className="container" key={item.id}>
+					<div className="row">
+						<div
+							className="col-12"
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "center",
+								alignItems: "center",
+								textAlign: "center",
+							}}
+						>
+							<div
+								class="card"
+								style={{
+									width: "18rem",
+									display: "flex",
+									flexDirection: "column",
+									justifyContent: "center",
+									alignItems: "center",
+									textAlign: "center",
+								}}
+							>
+								<img src={item.img} class="card-img-top" alt="..." />
+								<div class="card-body">
+									<h5 class="card-title">{item.cate}</h5>
+									<p class="card-text">{item.title}.</p>
+									<a href="#" class="btn btn-primary">
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			))}
+
+			{/* {product.data.map((item) => (
+				<div className="container">
+					<div className="row">
+						<div
+							className="col-12"
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "center",
+								alignItems: "center",
+								textAlign: "center",
+							}}
+						>
+							<div
+								class="card"
+								style={{
+									width: "18rem",
+									display: "flex",
+									flexDirection: "column",
+									justifyContent: "center",
+									alignItems: "center",
+									textAlign: "center",
+								}}
+							>
+								<img src={item.img} class="card-img-top" alt="..." />
+								<div class="card-body">
+									<h5 class="card-title">{item.cate}</h5>
+									<p class="card-text">{item.title}.</p>
+									<a href="#" class="btn btn-primary">
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			))} */}
+		</>
+	);
+};
+
+export default Carts;
---
+++ src/data.js
@@ -0,0 +1,47 @@
+import head1 from "./images/1.png";
+import head2 from "./images/2.png";
+import head3 from "./images/3.png";
+
+const product = {
+	data: [
+		{
+			id: 1,
+			img: head1,
+			cate: "play 1",
+			title: "headphones 1",
+		},
+		{
+			id: 2,
+			img: head2,
+			cate: "play 2",
+			title: "headphones 2",
+		},
+		{
+			id: 3,
+			img: head3,
+			cate: "play 3",
+			title: "headphones 3",
+		},
+	],
+	data1: [
+		{
+			id: 1,
+			img: head1,
+			cate: "asdas 1",
+			title: "headphones 1",
+		},
+		{
+			id: 2,
+			img: head2,
+			cate: "asdasd 2",
+			title: "headphones 2",
+		},
+		{
+			id: 3,
+			img: head3,
+			cate: "sdfsdf 3",
+			title: "headphones 3",
+		},
+	],
+};
+export default product;
