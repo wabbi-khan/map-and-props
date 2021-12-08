b9be70208d8b63ef50c22b54e7a9bf1f9b4f9a1b
Wahab Khan <kwahab789@gmail.com>
2021-12-09 03:20:15 +0500

    axios

--- src/components/ApiAxios.jsx
+++ src/components/ApiAxios.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Axios from "axios";
 const ApiAxios = () => {
-	return <div>hello</div>;
+	const [joke, setjoke] = useState([]);
+	const getJoke = async () => {
+		const result = await Axios.get("https://v2.jokeapi.dev/joke/Any?amount=5");
+		console.log(
+			result.data.jokes[0].setup + "..." + result.data.jokes[0].delivery
+		);
+
+		setjoke(result.data.jokes[0].setup + "..." + result.data.jokes[0].delivery);
+		// setjoke(result.data.jokes.setup);
+		// setjoke(result.joke.setup);
+	};
+
+	return (
+		<div>
+			hello <button onClick={getJoke}>jokes</button>
+			{joke}
+		</div>
+	);
 };
 
 export default ApiAxios;
